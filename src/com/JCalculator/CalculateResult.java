19a3c68029c9a7e248a8a76af712a5cbce413230
UAndreyV <andreyusa08@gmail.com>
2016-10-01 14:02:23 +0300

    Fixed bug

--- src/com/JCalculator/CalculateResult.java
+++ src/com/JCalculator/CalculateResult.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 import static com.JCalculator.Calculator.*;
 
 public class CalculateResult extends ExtendsMathFormul {
-//there is a serious bug in the calculations, will soon fix
     public void result(String resultation) throws IOException {
         Pattern p = Pattern.compile("[0-9,+,-,*,/]+");
         Matcher m = p.matcher(example);
@@ -30,10 +29,6 @@ public class CalculateResult extends ExtendsMathFormul {
                 singTestR++;
             }
 
-            if ((cList.get(i) == '*' || cList.get(i) == '/') && cList.get(i + 1) == '-') {
-                cList.remove(i + 1);
-                singTest++;
-            }
 
         }
         StringBuilder out = new StringBuilder(cList.size());
@@ -42,22 +37,44 @@ public class CalculateResult extends ExtendsMathFormul {
             out.append(c);
         }
         example = out.toString();
+
+
         String[] result = example.split("(-|\\+|\\*|\\/)");
+
+        ArrayList<Integer> singList = new ArrayList<Integer>();
+
+
         if (singTestR > 0)
             result[0] = "-" + result[0];
-        for (int i = 0; i < result.length; i++) {
-
-            listNumbers.add(result[i]);
-        }
-
-        for (int i = 0; i < listNumbers.size(); i++) {
-            if (listNumbers.get(i).equals("")) listNumbers.remove(i);
-        }
-
 
         for (int i = 0; i < example.length(); i++) {
             if (example.charAt(i) == '+' || example.charAt(i) == '-' || example.charAt(i) == '*' || example.charAt(i) == '/')
                 listOperations.add(Character.toString(example.charAt(i)));
+
+        }
+
+        for (int i = 0; i < listOperations.size() - 1; i++) {
+            if ((listOperations.get(i).equals("/") && listOperations.get(i + 1).equals("-")) || (listOperations.get(i).equals("*") && listOperations.get(i + 1).equals("-"))) {
+                singList.add(i + 2);
+                listOperations.remove(i + 1);
+            }
+
+        }
+
+
+        for (int i = 0; i < result.length; i++) {
+
+            listNumbers.add(result[i]);
+        }
+        if (!singList.isEmpty()) {
+            for (int i = 0; i < singList.size(); i++) {
+                listNumbers.set(singList.get(i), "-" + listNumbers.get(singList.get(i)));
+
+            }
+        }
+
+        for (int i = 0; i < listNumbers.size(); i++) {
+            if (listNumbers.get(i).isEmpty()) listNumbers.remove(i);
         }
 
 
